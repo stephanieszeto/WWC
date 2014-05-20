366d2fd7dbbee4f0e4fe54f72ce196e11fd0d4fd
Stephanie Szeto <stephaniexszeto@gmail.com>
2014-05-19 19:58:54 -0700

    Initial Commit

---
+++ WWCTests/WWCTests.m
@@ -0,0 +1,34 @@
+//
+//  WWCTests.m
+//  WWCTests
+//
+//  Created by Stephanie Szeto on 5/19/14.
+//  Copyright (c) 2014 projects. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WWCTests : XCTestCase
+
+@end
+
+@implementation WWCTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
